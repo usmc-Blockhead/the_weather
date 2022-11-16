8ad448c8963d212cdfb580a2ee585f0b1db22739
usmc-Blockhead <jaysplaypen@gmail.com>
2022-11-15 16:10:20 -0800

    add Fahrenheit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # the_weather
+
+![1668551247635](image/README/1668551247635.png)
--- weather.py
+++ weather.py
@@ -4,15 +4,17 @@ API_KEY = "ac79e3ae09bc111a8f43be1a1899d920"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}&units=imperial"
 response = requests.get(request_url)
 
 if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
-    temperature = round(data["main"]["temp"] - 273.15, 2)
+    ftemperature = round(data["main"]["temp"])
+    ctemperature = round((data["main"]["temp"] - 32) * 5/9)
 
     print("Weather:", weather)
-    print("Temperature:", temperature, "celsius")
+    print("Temperature:", ftemperature, "Fahrenheit")
+    print("Temperature:", ctemperature, "celsius")
 else:
     print("An error occurred.")
